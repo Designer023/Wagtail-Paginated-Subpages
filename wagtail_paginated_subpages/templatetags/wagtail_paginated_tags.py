d33466cc981fc1a9a126de10be2e9afda795dd73
Carl Topham <carl@carl-topham.com>
2018-02-06 09:46:28 +0000

    Refactored functions to reduce duplication

--- wagtail_paginated_subpages/templatetags/wagtail_paginated_tags.py
+++ wagtail_paginated_subpages/templatetags/wagtail_paginated_tags.py
@@ -5,13 +5,18 @@ from .. import settings as app_settings
 register = template.Library()
 
 
-@register.inclusion_tag('wagtail_paginated_subpages/pagination.html', takes_context=True)
 def render_pagination(context, page, paginated_objects):
+    """
+    Manages the context and logic to create the necessary pagination
+    :param context: page context
+    :param page: Page: The root page with the child pages
+    :param paginated_objects: List of paginated sub page objects
+    :return: template vars to create paginated template
+    """
     request = context['request']
     current_page_size = context['current_page_size']
 
     # Load app settings
-    redirect_index_to_root = app_settings.PAGINATION_REDIRECT_INDEX_TO_ROOT
     default_page_sizes = app_settings.PAGINATION_DEFAULT_PAGE_SIZES
     default_page_size = app_settings.PAGINATION_PAGE_SIZE_DEFAULT
 
@@ -23,7 +28,7 @@ def render_pagination(context, page, paginated_objects):
 
     return {
         'request': request,
-        'page':page,
+        'page': page,
         'paginated_objects': paginated_objects,
         'default_page_size': default_page_size,
         'default_page_sizes': default_page_sizes,
@@ -33,36 +38,45 @@ def render_pagination(context, page, paginated_objects):
     }
 
 
-@register.inclusion_tag('wagtail_paginated_subpages/pagination_meta.html', takes_context=True)
-def render_pagination_meta(context, page, paginated_objects):
-    request = context['request']
-    current_page_size = context['current_page_size']
-
-    # Load app settings
-    default_page_size = app_settings.PAGINATION_PAGE_SIZE_DEFAULT
-
-    render_prev_next = True
-    if current_page_size != default_page_size:
-        # Non standard so alert SEO that page is non indexed and google
-        render_prev_next = False
-
-    return {
-        'request': request,
-        'page':page,
-        'paginated_objects': paginated_objects,
-        'render_prev_next': render_prev_next
-    }
-
-
-@register.simple_tag(takes_context=True)
-def paginationpageurl(context, page, url_name, page_id, qs, *args, **kwargs):
-
+def paginationpageurl(context, page, url_name, page_id, qs):
+    """
+    Enhanced version  of wagtails own 'routablepageurl' that adds handling
+    for the routing of page 1 and
+    :param context: page context
+    :param page: Page: The root page with the child pages
+    :param url_name: sting: the url named pattern for paginated pages
+    :param page_id: int: he paginated page number
+    :param qs: string: Optional query string for handling page sizes
+    :return: string: url for the paginated page
+    """
     request = context['request']
     base_url = page.relative_url(request.site)
 
+    # Check if /page/1/ is being routed to the root page or
+    # if root page is routed to /page/1/ and set the sub_page_url
     if app_settings.PAGINATION_REDIRECT_INDEX_TO_ROOT and page_id == 1:
-        routed_url = ''
+        sub_page_url = ''
     else:
-        routed_url = page.reverse_subpage(url_name, args=(page_id,))
+        sub_page_url = page.reverse_subpage(url_name, args=(page_id,))
 
-    return base_url + routed_url + qs
+    # Return full page url with sub page routing and querystring
+    return base_url + sub_page_url + qs
+
+
+register.inclusion_tag(
+    'wagtail_paginated_subpages/pagination.html',
+    takes_context=True,
+    name='render_pagination'
+)(render_pagination)
+
+
+register.inclusion_tag(
+    'wagtail_paginated_subpages/pagination_meta.html',
+    takes_context=True,
+    name='render_pagination_meta'
+)(render_pagination)
+
+
+register.simple_tag(
+    takes_context=True
+)(paginationpageurl)
